83b5b11b7e093022966607ed040efa837294387c
Dimitar Pashev <dim.pashev@gmail.com>
2024-06-06 13:40:59 +0300

    add all functionalities and screenshot

--- Database App/demo1.py
+++ Database App/demo1.py
@@ -1,5 +1,62 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
+import psycopg2
+
+def run_query(query, parameters=()):
+    con = psycopg2.connect(dbname="studentdb", user="postgres", password="parolata22", host="localhost", port="5432")
+    cur = con.cursor()
+    query_result = None
+    try:
+        cur.execute(query, parameters)
+        if query.lower().startswith("select"):
+            query_result = cur.fetchall()
+        con.commit()
+    except psycopg2.Error as e:
+        messagebox.showerror("Database Error", str(e))
+    finally:
+        cur.close()
+        con.close()
+    return query_result
+
+def refresh_treeview():
+    for item in tree.get_children():
+        tree.delete(item)
+    records = run_query("select * from students;")
+    for record in records:
+        tree.insert('', END, values=record)
+
+
+def insert_data():
+    query = "insert into students(name, address, age, number) values (%s, %s, %s, %s)"
+    parameters = (name_entry.get(), address_entry.get(), age_entry.get(), phone_entry.get())
+    run_query(query, parameters)
+    messagebox.showinfo("Information", "Data inserted ")
+    refresh_treeview()
+
+def delete_data():
+    selected_item = tree.selection()[0]    
+    student_id = tree.item(selected_item)['values'][0]
+    query = "delete from students where student_id=%s"
+    parameters = (student_id,)
+    run_query(query, parameters)
+    messagebox.showinfo("Information", "Data deleted successfully")
+    refresh_treeview()
+
+def update_data():
+    selected_item = tree.selection()[0]    
+    student_id = tree.item(selected_item)['values'][0]
+    query = "update students set name=%s, address=%s, age=%s, number=%s where student_id=%s"
+    parameters = (name_entry.get(), address_entry.get(), age_entry.get(), phone_entry.get(), student_id)
+    run_query(query, parameters)
+    messagebox.showinfo("Information", "Data updated successfully")
+    refresh_treeview()
+
+def create_table():
+    query = "create table if not exists students(student_id serial primary key, name text, address text, age int, number text);"
+    run_query(query)
+    messagebox.showinfo("Information", "Table created successfully")
+    refresh_treeview()
 
 root = Tk()
 root.title("Student management system")
@@ -28,10 +85,10 @@ phone_entry.grid(row=3, column=1, pady=2, sticky="ew")
 button_frame = Frame(root)
 button_frame.grid(row=1, column=0, pady=5, sticky="ew")
 
-Button(button_frame, text="Create Table").grid(row=0, column=0, padx=5)
-Button(button_frame, text="Add Data").grid(row=0, column=1, padx=5)
-Button(button_frame, text="Update Data").grid(row=0, column=2, padx=5)
-Button(button_frame, text="Delete Data").grid(row=0, column=3, padx=5)
+Button(button_frame, text="Create Table", command=create_table).grid(row=0, column=0, padx=5)
+Button(button_frame, text="Add Data", command=insert_data).grid(row=0, column=1, padx=5)
+Button(button_frame, text="Update Data", command=update_data).grid(row=0, column=2, padx=5)
+Button(button_frame, text="Delete Data", command=delete_data).grid(row=0, column=3, padx=5)
 
 
 tree_frame = Frame(root)
@@ -58,5 +115,7 @@ tree.heading("address", text="address", anchor=CENTER)
 tree.heading("age", text="age", anchor=CENTER)
 tree.heading("number", text="number", anchor=CENTER)
 
+
+refresh_treeview()
 
 root.mainloop()
