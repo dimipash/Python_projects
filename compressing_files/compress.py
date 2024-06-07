e005e4985bbf0e3d33d67f20562111dc1540163a
Dimitar Pashev <dim.pashev@gmail.com>
2024-06-07 19:07:42 +0300

    add decompress functionality

--- compressing_files/compress.py
+++ compressing_files/compress.py
@@ -6,3 +6,6 @@ compressed_data = base64.b64encode(zlib.compress(data_bytes, 9))
 decoded_data = compressed_data.decode('utf-8')
 compressed_file = open('compressed.txt', 'w')
 compressed_file.write(decoded_data)
+
+decompresses_data = zlib.decompress(base64.b64decode(compressed_data))
+print(decompresses_data)
