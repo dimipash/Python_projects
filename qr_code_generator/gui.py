bf4c05aa0485044f0a6a4360fbbc5129306a3dc3
Dimitar Pashev <dim.pashev@gmail.com>
2024-06-11 09:23:12 +0300

    add functionality and screenshot

--- qr_code_generator/gui.py
+++ qr_code_generator/gui.py
@@ -4,6 +4,19 @@ from PIL import ImageTk, Image
 
 root = Tk()
 
+def generate():
+    link_name = name_entry.get()
+    link = link_entry.get()
+    file_name = link_name + ".png"
+    url = pyqrcode.create(link)
+    url.png(file_name, scale=8)
+    image = ImageTk.PhotoImage(Image.open(file_name))
+    image_label = Label(image=image)
+    image_label.image = image
+    canvas.create_window(200, 250, window=image_label)
+
+
+
 canvas = Canvas(root, width=400, height=600)
 canvas.pack()
 
@@ -23,7 +36,7 @@ link_entry = Entry(root)
 canvas.create_window(200, 130, window=name_entry)
 canvas.create_window(200, 180, window=link_entry)
 
-button = Button(text="Generate QR Code")
+button = Button(text="Generate QR Code", command=generate)
 canvas.create_window(200, 230, window=button)
 
 
