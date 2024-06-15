4a0a5613d6bbfc7de9d33d179f95644a30ffdf09
Dimitar Pashev <dim.pashev@gmail.com>
2024-06-15 16:34:19 +0300

    getting local path

--- youtube_video_downloader/demo.py
+++ youtube_video_downloader/demo.py
@@ -1,6 +1,12 @@
 from tkinter import *
 from tkinter import filedialog
 
+
+
+def get_path():
+    path = filedialog.askdirectory()
+    path_label.config(text=path)
+
 root = Tk()
 root.title('Video Downloader')
 canvas = Canvas(root, width=400, height=300)
@@ -18,7 +24,7 @@ canvas.create_window(200, 100, window=url_entry)
 
 
 path_label = Label(root, text="Select path to download")
-path_button = Button(root, text="Select")
+path_button = Button(root, text="Select", command=get_path)
 canvas.create_window(200, 150, window=path_label)
 canvas.create_window(200, 170, window=path_button)
 
