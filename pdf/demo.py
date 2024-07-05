17f87349ec347759089236e0e94b2545d1c5cc90
Dimitar Pashev <dim.pashev@gmail.com>
2024-07-05 13:30:59 +0300

    adding multiple lines

--- pdf/demo.py
+++ pdf/demo.py
@@ -6,11 +6,14 @@ class PDF(FPDF):
         self.set_font("helvetica", "B", 16)
         self.cell(80)
         self.cell(40, 10, "Hello, world", border=1,align="C")
+        self.ln(40)
 
 
 pdf = PDF()
-
 pdf.add_page()
+pdf.set_font("helvetica", "B", 16)
+for i in range(1, 41):
+    pdf.cell(0, 10, f"Printing line number {i}", new_x="LMARGIN", new_y="NEXT")
 
 
 pdf.output("sample.pdf")
