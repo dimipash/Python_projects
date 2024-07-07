3cd1f8793fddcd982e13cb274758fe1d03d02d71
Dimitar Pashev <dim.pashev@gmail.com>
2024-07-07 11:32:11 +0300

    add medicines to cart

--- invoice_generator/demo.py
+++ invoice_generator/demo.py
@@ -3,10 +3,26 @@ from tkinter import *
 window = Tk()
 window.title("Invoice Generator")
 
+medicines = {"Aspirin": 15, "Ibuprofen": 20, "Paracetamol": 5, "Acetaminophen": 10}
+
+invoice_items = []
+
+
+def add_medicine():
+    selected_medicine = medicine_listbox.get(ANCHOR)
+    quantity = int(quantity_entry.get())
+    price = medicines[selected_medicine]
+    item_total = price * quantity
+    invoice_items.append((selected_medicine, quantity, item_total))
+    print(invoice_items)
+
+
 medicine_label = Label(window, text="Medicine:")
 medicine_label.pack()
 
 medicine_listbox = Listbox(window, selectmode=SINGLE)
+for medicine in medicines:
+    medicine_listbox.insert(END, medicine)
 medicine_listbox.pack()
 
 quantity_label = Label(window, text="Quantity:")
@@ -14,7 +30,7 @@ quantity_label.pack()
 quantity_entry = Entry(window)
 quantity_entry.pack()
 
-add_button = Button(window, text="Add Medicine")
+add_button = Button(window, text="Add Medicine", command=add_medicine)
 add_button.pack()
 
 total_amount_label = Label(window, text="Total Amount")
@@ -32,7 +48,7 @@ customer_entry.pack()
 generate_button = Button(window, text="Generate Invoice")
 generate_button.pack()
 
-invoice_text = Text(window, width=10, height=50)
+invoice_text = Text(window, width=40, height=40)
 invoice_text.pack()
 
 window.mainloop()
