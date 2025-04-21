e75ce8b289b88f8702a1c8bf945054ccc07919c4
Dimitar Pashev <dim.pashev@gmail.com>
2025-04-21 18:16:22 +0300

    initial commit

---
+++ automate_finances/main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import json
+import os
+
+st.set_page_config(page_title="Simple Finance App", page_icon="💵", layout="wide")
+
